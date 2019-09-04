baad6cc64496bb03c54e8daac4eb1fc478c8dc3d
Cory <coadams9@gmail.com>
2019-09-04 18:35:45 -0400

    test

---
+++ src/components/LandingPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./landingPage.scss";
+
+function LandingPage() {
+  return (
+    <div className="landingPage">
+      <div className="background">
+        <h1 style={{ color: "red" }}>CORY</h1>
+      </div>
+    </div>
+  );
+}
+
+export default LandingPage;
