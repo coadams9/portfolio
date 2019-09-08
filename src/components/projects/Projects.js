5dc57533bda95e4243a2e136aa69c7b0b34eef25
Cory <coadams9@gmail.com>
2019-09-07 20:28:10 -0400

    changed video walkthrough link

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -15,7 +15,7 @@ function Projects() {
                 </div></a>
                 <div className='description'>
                     {tad}
-                    <a href='https://www.linkedin.com/in/adamsrcory/detail/treasury/summary/?entityUrn=urn%3Ali%3Afs_treasuryMedia%3A(ACoAAB8i-QoBWh076p4H_OIFqg9vWfVrAEexroY%2C1567178667946)&section=summary&treasuryCount=3&lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3ByvWF4B9hQGST8FEg2z1oPA%3D%3D&licu=urn%3Ali%3Acontrol%3Ad_flagship3_profile_view_base-treasury_thumbnail_cell'>Video Walkthrough</a>
+                    <a href='https://youtu.be/V4XQn1TsRPQ'>Video Walkthrough</a>
                 </div>
             </div>
             <div className='project'>
