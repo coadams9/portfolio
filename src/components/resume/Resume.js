7c068e36414591b4b3d41ffc3a29bb0d4971d3c9
Cory <coadams9@gmail.com>
2019-09-18 15:32:44 -0400

    popups

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -7,7 +7,7 @@ import './resume.css'
 
 function Resume() {
     return (
-        <iframe sandbox="allow-popups-to-escape-sandbox, allow-popups" src="https://docs.google.com/document/d/e/2PACX-1vSEmE_mCemtQoZwZBYvlX9gJjXpjEK3FTDkb82wulpZiNIIju6EVOlzHb45fSKdQLfh_3B9S4eEdx8b/pub?embedded=true"></iframe>
+        <iframe sandbox="allow-popups-to-escape-sandbox" src="https://docs.google.com/document/d/e/2PACX-1vSEmE_mCemtQoZwZBYvlX9gJjXpjEK3FTDkb82wulpZiNIIju6EVOlzHb45fSKdQLfh_3B9S4eEdx8b/pub?embedded=true"></iframe>
     )
 }
 
